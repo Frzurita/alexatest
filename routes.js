8bf0d73332a0c9e9881186e53e1af198bcb77e4c
Frzurita <zurit92@gmail.com>
2016-07-20 13:29:08 +0200

    trying to connect with alexa

--- routes.js
+++ routes.js
@@ -5,19 +5,32 @@ var _ = require('underscore');
 var request = require('request');
 var qs = require('querystring');
 var alexa_app = require('alexa-app');
-var alexa = new alexa_app.app('sample');
+var alexa = new alexa_app.app('SmartHome');
 
-alexa.intent('number',
+alexa.intent('MySensorIsIntent',
     {
-        "slots":{"number":"NUMBER"}
+        "slots":{
+            "name":"DeviceSensor",
+            "type":"LIST_OF_SENSOR"
+        }
         ,"utterances":[ "say the number {1-100|number}" ]
+    },
+    function(request,response) {
+        var number = request.slot('name');
+        response.say("You asked for the number "+ number);
+    }
+);
+
+
+alexa.intent('',
+    {
+        "utterances":[ "say the number {1-100|number}" ]
     },
     function(request,response) {
         var number = request.slot('number');
         response.say("You asked for the number "+number);
     }
 );
-
 
 /* GET home_page. */
 router.get('/', function(req, res, next) {
@@ -29,6 +42,7 @@ router.get('/api/lights', function (req, res, next) {
 });
 
 router.post('/api/alexa',function(req,res) {
+    console.log(req.body);
     alexa.request(req.body)        // connect express to alexa-app
         .then(function(response) { // alexa-app returns a promise with the response
             res.json(response);      // stream it to express' output
