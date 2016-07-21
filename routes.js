dbf4e0d36d080f5c24e3947ef25a1a5341954791
Frzurita <zurit92@gmail.com>
2016-07-21 12:34:08 +0200

    building a happyBithday intent

--- routes.js
+++ routes.js
@@ -40,7 +40,7 @@ alexa.intent('HappyBirthdayIntent',
         var name = request.slot("birthName");
         var age = request.slot("age");
         console.log(name);
-        response.say("Hello " + name + ". I'm Alexa, I wish you a great day " + name +". I wish you a very happy " + age + "th year.");
+        response.say("Hello " + name + ". I'm Alexa, I wish you a very happy birthday. "+ age +" is a great age to do awesome stuff.");
     }
 );
 
