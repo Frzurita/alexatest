f1a79031bc8f6f7a7b8268358c83eb43700e6118
Miguel Jimenez <migueljiarr@debian>
2016-11-14 13:28:15 +0100

    Creados certificados para conectarnos a Alexa

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+var https = require('https');
+var fs = require('fs');
+	pem = require('pem');
+
+var options = {
+      key: fs.readFileSync('certs/private-key.pem'),
+      cert: fs.readFileSync('certs/certificate.pem'),
+      requestCert: false,
+      rejectUnauthorized: false
+};
+pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
+
+var server = https.createServer(options, app).listen(3000, function(){
+    console.log("server started at port 3000");
+});
+});
+
+
 var express = require('express');
 var app = express();
+
+
 var router = require('./app');
 
 app.set('port', process.env.PORT || 8000);
