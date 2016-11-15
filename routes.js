4e97117868cd333e675aeef1a51c17cbd03d9365
root <becSmart@gmail.com>
2016-11-15 18:32:56 +0000

    some changes done

--- routes.js
+++ routes.js
@@ -9,99 +9,100 @@ var alexa = new alexa_app.app('test');
 var smartplugStatus = require('./smartplugStatus.json');
 var ifttt = require('./lib/ifttp');
 var fs = require('fs');
-//
-// alexa.launch(function(request,response) {
-//     response.say("Hello, welcome to the hell").shouldEndSession(false)
-//         .reprompt('Pepe mama facking');
-// });
-//
-// alexa.intent('MySensorIsIntent',
-//     {
-//         "slots":[{
-//             "name":"DeviceSensor",
-//             "type":"LIST_OF_SENSOR"
-//         }]
-//     },
-//     function(request,response) {
-//         console.log('Estoy en slot');
-//         var number = request.slot();
-//         response.say("You asked for the number ");
-//     }
-// );
-//
-// ifttt();
-//
-//
-// alexa.intent('HappyBirthdayIntent',
-//     {
-//         "slots":{
-//             "name":"NAME",
-//             "value":"VALUE"
-//         }
-//     },
-//     function(request,response) {
-//         console.log('Estoy en slot');
-//         var name = request.slot("birthName");
-//         var age = request.slot("age");
-//         console.log(name);
-//         response.say("Hello " + name + ". I'm Alexa, I wish you a very happy birthday. "+ age +" is a great age to do awesome stuff.");
-//     }
-// );
-//
-// alexa.intent('turnTheLightOnIntent',
-//     {
-//         "slots":{
-//             "name":"NAME",
-//             "value":"VALUE"
-//         }
-//     },
-//     function(request,response) {
-//         var state = request.slot("light_state");
-//         var name = request.slot("light_name");
-//         var send_light = {};
-//
-//         requested
-//             .get({url: req.gw_ip + 'lights', qs: '', json: true},function(err, res, body) {
-//                 response.body = lights;
-//                 if(response.statusCode == 200){
-//                     if(state == "on") {
-//                         lights[0].state.on = true;
-//                     }
-//                     else if(state == "off"){
-//                         lights[0].state.on = false;
-//                     }
-//
-//
-//                     requested
-//                         .post({url: req.gw_ip + 'updateLight', form: JSON.stringify(lights[0]), json: true},function(err, res, body) {
-//                             if(response.statusCode == 200){
-//                                 console.log("I'm working");
-//                                 response.say("The light  "+ name + "is " + state);
-//                             }// 200
-//                             else{
-//                                 res.json({msg:'me conecto pero sin fallo'});
-//                             }
-//                             console.log(response.headers['content-type']); // 'image/png'
-//                         });
-//                 }// 200
-//                 else{
-//                     res.json({msg:'me conecto pero sin fallo'});
-//                 }// 'image/png'
-//
-//             });
-//     }
-// );
-//
-//
-// alexa.intent('MyLightIsIntent',
-//     {
-//         "utterances":[ "say the number {1-100|number}" ]
-//     },
-//     function(request,response) {
-//         var number = request.slot('number');
-//         response.say("You asked for the number "+number);
-//     }
-// );
+
+ alexa.launch(function(request,response) {
+	console.log("launching alexa\n");
+     response.say("Hello, welcome to the hell").shouldEndSession(false)
+         .reprompt('Pepe mama facking');
+ });
+
+ alexa.intent('MySensorIsIntent',
+     {
+         "slots":[{
+             "name":"DeviceSensor",
+             "type":"LIST_OF_SENSOR"
+         }]
+     },
+     function(request,response) {
+         console.log('Estoy en slot');
+         var number = request.slot();
+         response.say("You asked for the number ");
+     }
+ );
+
+ //ifttt();
+
+
+ alexa.intent('HappyBirthdayIntent',
+     {
+         "slots":{
+             "name":"NAME",
+             "value":"VALUE"
+         }
+     },
+     function(request,response) {
+         console.log('Estoy en slot');
+         var name = request.slot("birthName");
+         var age = request.slot("age");
+         console.log(name);
+         response.say("Hello " + name + ". I'm Alexa, I wish you a very happy birthday. "+ age +" is a great age to do awesome stuff.");
+     }
+ );
+
+ alexa.intent('turnTheLightOnIntent',
+     {
+         "slots":{
+             "name":"NAME",
+             "value":"VALUE"
+         }
+     },
+     function(request,response) {
+         var state = request.slot("light_state");
+         var name = request.slot("light_name");
+         var send_light = {};
+
+         requested
+             .get({url: req.gw_ip + 'lights', qs: '', json: true},function(err, res, body) {
+                 response.body = lights;
+                 if(response.statusCode == 200){
+                     if(state == "on") {
+                         lights[0].state.on = true;
+                     }
+                     else if(state == "off"){
+                         lights[0].state.on = false;
+                     }
+
+
+                     requested
+                         .post({url: req.gw_ip + 'updateLight', form: JSON.stringify(lights[0]), json: true},function(err, res, body) {
+                             if(response.statusCode == 200){
+                                 console.log("I'm working");
+                                 response.say("The light  "+ name + "is " + state);
+                             }// 200
+                             else{
+                                 res.json({msg:'me conecto pero sin fallo'});
+                             }
+                             console.log(response.headers['content-type']); // 'image/png'
+                         });
+                 }// 200
+                 else{
+                     res.json({msg:'me conecto pero sin fallo'});
+                 }// 'image/png'
+
+             });
+     }
+ );
+
+
+ alexa.intent('MyLightIsIntent',
+     {
+         "utterances":[ "say the number {1-100|number}" ]
+     },
+     function(request,response) {
+         var number = request.slot('number');
+         response.say("You asked for the number "+number);
+     }
+ );
 
 router.post('/', function(req, res, next) {
     res.render('index.html');
@@ -159,6 +160,51 @@ router.post('/api/alexa',function(req,res) {
 router.get('/api/alexa', function (req, res, next) {
     console.log("I'm a get function");
     res.json({msg: "I'm a get method"});
+    //res.json(buildSpeechletResponse());
 });
+
+function buildSpeechletResponse() {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: "output"
+        },
+        card: {
+            type: "Simple",
+            title: "title",
+            content: "output"
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: "repromptText"
+            }
+        },
+        shouldEndSession: false
+    };
+}
+
+/*
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+*/
 
 module.exports = router;
