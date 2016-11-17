2041dcfcb23a89d37b54afbcffb8eb8d8ae58f2c
root <becSmart@gmail.com>
2016-11-17 13:52:09 +0100

    Funcionando con HTTPS y Alexa

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-var https = require('https');
-var fs = require('fs');
-	pem = require('pem');
-
-var options = {
-      key: fs.readFileSync('certs/private-key.pem'),
-      cert: fs.readFileSync('certs/certificate.pem'),
-      requestCert: false,
-      rejectUnauthorized: false
-};
-pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
-
-var server = https.createServer(options, app).listen(3000, function(){
-    console.log("server started at port 3000");
-});
-});
-
-
 var express = require('express');
 var app = express();
 
+var https = require('https');
+var fs = require('fs');
 
 var router = require('./app');
 
-app.set('port', process.env.PORT || 8000);
+//app.set('port', process.env.PORT || 8000);
+var port = process.env.PORT || 8000;
+
+var sslPath = '/etc/letsencrypt/live/becsmarthome.dnsdynamic.com/';
+
+var options = {  
+    key: fs.readFileSync(sslPath + 'privkey.pem'),
+    cert: fs.readFileSync(sslPath + 'fullchain.pem')
+};
+
 
 app.use(router);
 
-app.listen(app.get('port'),'0.0.0.0');
+server = https.createServer(options, app);  
+io = require('socket.io').listen(server);  
+server.listen(443);  
+//app.listen(443);
+app.listen(port);
+//app.listen(app.get('port'),'0.0.0.0');
 
 console.log('servidor funcionando por el puerto' + app.get('port'));
+console.log('servidor funcionando por el puerto' + port);
+console.log('servidor funcionando por el puerto' + 443);
